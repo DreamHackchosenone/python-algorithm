bcd6f21f0ef453d42b5db955a0d09f21176125b1
chosenoneMAC <15521370865@163.com>
2019-03-19 17:53:07 +0800

    简单选择排序

--- sort_algorithm/select_sort.py
+++ sort_algorithm/select_sort.py
@@ -1,14 +1,23 @@
-def select_sort(list):
-    for i in range(len(list)):
-        least = list[i]
-        j = i+1
-        while j< len(list):
-            if least > list[j]:
-                temp = least
-                least = list[j]
-                list[j] = temp
+# coding:utf-8
+
+# 选择排序
+# 时间复杂度 n*n
+# 每次选择最大的放在最后
+
+
+def select_sort(test_list):
+    for i in range(len(test_list)):
+        least = test_list[i]
+        j = i + 1
+        while j < len(test_list):
+            if least > test_list[j]:
+                least, test_list[j] = test_list[j], least
             j += 1
-        list[i] = least
-    print(list[::])
+            test_list[i] = least
+    return test_list
+
+
 a = [1, 4, 2, 7, 3]
-select_sort(a)
+print(select_sort(a))
+
+
